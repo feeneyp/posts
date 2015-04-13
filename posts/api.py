2d72eadd643c66f94f24771f78fbc0ee368e96a3
feeneyp <patrick@r3dpixel.com>
2015-04-13 16:50:24 +0000

    initial commit

--- posts/api.py
+++ posts/api.py
@@ -8,3 +8,16 @@ import decorators
 from posts import app
 from database import session
 
+@app.route("/api/posts", methods=["GET"])
+def posts_get():
+    """ Get a list of posts """
+
+    # Get the posts from the database
+    posts = session.query(models.Post).all()
+
+    # Convert the posts to JSON and return a response
+    data = json.dumps([post.as_dictionary() for post in posts])
+    return Response(data, 200, mimetype="application/json")
+    
+  
+  
--- tests/api_tests.py
+++ tests/api_tests.py
@@ -25,6 +25,32 @@ class TestAPI(unittest.TestCase):
         session.close()
         # Remove the tables and their data from the database
         Base.metadata.drop_all(engine)
+        
+        
+    def testGetPosts(self):
+        """ Getting posts from a populated database """
+        postA = models.Post(title="Example Post A", body="Just a test")
+        postB = models.Post(title="Example Post B", body="Still a test")
+
+        session.add_all([postA, postB])
+        session.commit()
+        
+        response = self.client.get("/api/posts")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.mimetype, "application/json")
+
+        data = json.loads(response.data)
+        self.assertEqual(len(data), 2)
+
+        postA = data[0]
+        self.assertEqual(postA["title"], "Example Post A")
+        self.assertEqual(postA["body"], "Just a test")
+
+        postB = data[1]
+        self.assertEqual(postB["title"], "Example Post B")
+        self.assertEqual(postB["body"], "Still a test")
+        
 
 if __name__ == "__main__":
     unittest.main()
